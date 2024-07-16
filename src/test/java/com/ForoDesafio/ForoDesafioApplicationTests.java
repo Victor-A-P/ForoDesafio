0c3313768c6cc81a510dda6c86ff0416ca390edd
Victor Alejandro Pacheco <victor.alejandro.ph@outlook.com>
2024-07-16 11:13:04 -0600

    Create proyect and upload for hithub

---
+++ src/test/java/com/ForoDesafio/ForoDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ForoDesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
